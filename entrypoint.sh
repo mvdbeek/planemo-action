e2ab76e6cde66c702d41a25f6f03942482d9a426
mvdbeek <m.vandenbeek@gmail.com>
2019-11-21 13:39:06 +0100

    Make sure planemo operates in the same paths as the host image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
+mkdir -p /home/runner/work/_temp/
+ln -s /github/home /home/runner/work/_temp/_github_home
+mkdir /home/runner/work/_temp/_github_home/tmp
+export TMPDIR=/home/runner/work/_temp/_github_home/tmp
+
 /planemo_venv/bin/planemo $@
