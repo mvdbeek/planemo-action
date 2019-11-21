0c2dc78c6c6cf86f0e4553ce2f27e5d2df6c615b
mvdbeek <m.vandenbeek@gmail.com>
2019-11-21 14:43:45 +0100

    Drop hardlink

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/sh -l
 
-mkdir -p /home/runner/work/_temp/
-ln /github/home /home/runner/work/_temp/_github_home
-mkdir /home/runner/work/_temp/_github_home/tmp
-export TMPDIR=/home/runner/work/_temp/_github_home/tmp
-
 /planemo_venv/bin/planemo $@
